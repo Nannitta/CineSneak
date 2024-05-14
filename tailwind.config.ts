88d641f0eb7df4779f721f39520f9c1940abe11f
Nanna <nanaa.gonzalez@gmail.com>
2024-05-14 10:49:35 +0200

    first steps

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,10 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      gray:"#D9D9D9"
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
     },
   },
   plugins: [],
