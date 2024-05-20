53187d050865a5634422cd2e23185d2fe0e37723
Nanna <nanaa.gonzalez@gmail.com>
2024-05-20 23:12:13 +0200

    Add sideMenu functionality for mobile devices

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Onest } from "next/font/google";
 import "./globals.css";
 import Header from './components/Header';
+import SideMenu from './components/SideMenu';
 import Footer from './components/Footer';
 
 const onest = Onest({ subsets: ["latin"] });
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en" className='min-h-screen'>
       <body className={`${onest.className} min-h-screen flex flex-col`}>
         <Header/>
+        <SideMenu/>
         {children}
         <Footer/>
       </body>
