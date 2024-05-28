57c08fcd431dcdc690c906a60287891d6cd4ac38
Nanna <nanaa.gonzalez@gmail.com>
2024-05-28 13:18:41 +0200

    Add popularMovies in moviesStore and styles for responsive devices

---
+++ src/app/components/horizontalCarousel/HorizontalCarousel.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import React from 'react'
+import { EmblaOptionsType } from 'embla-carousel'
+import useEmblaCarousel from 'embla-carousel-react'
+import "./horizontalCarousel.css"
+import { MoviesNowPalying } from '@/app/types/types'
+import HorizontalCardCarousel from './HorizontalCardMovie'
+
+type PropType = {
+  movies: MoviesNowPalying[]
+  options?: EmblaOptionsType
+}
+
+const HorizontalCarousel: React.FC<PropType> = (props) => { 
+  const { movies, options } = props;
+  const [emblaRef] = useEmblaCarousel(options);
+
+  return (
+    <section className="embla">
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container horizontal__container">
+          {movies.map((movie) => (
+            <div className="embla__slide horizontal__slide" key={movie.id}>
+              <HorizontalCardCarousel movie={movie}/>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default HorizontalCarousel
