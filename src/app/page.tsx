e8d66bdaaea85b9a1c018ee1be5d782faa2b3c8e
Nanna <nanaa.gonzalez@gmail.com>
2024-05-28 13:18:41 +0200

    Add popularMovies in moviesStore and styles for responsive devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,15 @@ import { useSideMenuStore } from './store/sideMenu';
 import WatchTrailer from './components/WatchTrailer';
 import VerticalCarousel from './components/verticalCarousel/VerticalCarousel';
 import { useSeriesStore } from './store/series';
+import HorizontalCarousel from './components/horizontalCarousel/HorizontalCarousel';
 
 export default function HomePageNotLog() {
+  const fecthUpcomingMovies = useMoviesStore(state => state.fetchUpcomingMovies);
+  const upcomingMovies = useMoviesStore(state => state.upcomingMovies);
   const fetchMoviesNowPlaying = useMoviesStore(state => state.fetchMoviesNowPlaying);
   const fetchGenres = useMoviesStore(state => state.fetchMoviesGenre);
   const fetchPopularMovies = useMoviesStore(state => state.fetchPopularMovies);
-  const moviesNowPlaying = useMoviesStore(state => state.moviesNowPlaying);  
+  const moviesNowPlayingStore = useMoviesStore(state => state.moviesNowPlaying);  
   const genres = useMoviesStore(state => state.movieGenres);
   const closeSideMenu = useSideMenuStore(state => state.closeSideMenu);
   const popularMoviesStore = useMoviesStore(state => state.popularMovies);
@@ -21,11 +24,13 @@ export default function HomePageNotLog() {
   const fetchPopularSeries = useSeriesStore(state => state.fetchPopularSeries);
 
   const OPTIONS: EmblaOptionsType = { loop: true }
-  const moviesOnTheatres = Array.from(moviesNowPlaying)
+  const moviesOnTheatres = Array.from(upcomingMovies)
+  const moviesNowPlaying = Array.from(moviesNowPlayingStore)
   const popularMovies = Array.from(popularMoviesStore)
   const popularSeries = Array.from(popularSeriesStore)
 
   useEffect(() => {
+    fecthUpcomingMovies()
     fetchMoviesNowPlaying();
     fetchGenres();
     fetchPopularMovies();
@@ -36,14 +41,24 @@ export default function HomePageNotLog() {
     <main className='flex-grow flex flex-col relative' onClick={closeSideMenu}>
       <WatchTrailer/>
       <CarouselOnTheatres moviesOnTheatres={moviesOnTheatres} options={OPTIONS} genres={genres}/>
-      <h2 className='font-black p-4 text-lg md:text-xl md:pt-[30px] md:pb-5 lg:text-2xl lg:pt-9 lg:pb-6 lg:px-6'>
-        Éxitos en taquilla ¡No te los pierdas!
-      </h2>
-      <VerticalCarousel movies={popularMovies}/>
-      <h2 className='font-black p-4 text-lg md:text-xl md:pt-[30px] md:pb-5 lg:text-2xl lg:pt-9 lg:pb-6 lg:px-6 text-balance'>
-        No te pierdas los últimos estrenos en series
-      </h2>
-      <VerticalCarousel movies={popularSeries}/>
+      <section>
+        <h2 className='font-black p-4 text-lg md:text-xl md:pt-[30px] md:pb-5 lg:text-2xl lg:pt-9 lg:pb-6 lg:px-6'>
+          Éxitos en taquilla ¡No te los pierdas!
+        </h2>
+        <VerticalCarousel movies={moviesNowPlaying}/>
+      </section>
+      <section className='flex flex-col md:flex-row md:items-center md:gap-2 md:pt-[30px] lg:pt-14 lg:gap-20'>
+        <h2 className='font-black py-4 pl-4 text-lg md:text-xl md:pt-0 md:pb-0 md:min-w-44 md:text-balance lg:text-2xl lg:px-6 lg:min-w-56'>
+          Descubre que películas son tendencia
+        </h2>
+        <HorizontalCarousel movies={popularMovies} />
+      </section>
+      <section>
+        <h2 className='font-black py-4 pl-4 text-lg md:text-xl md:pt-[30px] md:pb-5 lg:text-2xl lg:pt-9 lg:pb-6 lg:px-6 text-balance'>
+          No te pierdas los últimos estrenos en series
+        </h2>
+        <VerticalCarousel movies={popularSeries}/>
+      </section>
     </main>
   );
 };
--- src/app/services/index.tsx
+++ src/app/services/index.tsx
@@ -1,6 +1,17 @@
 require('dotenv').config();
 const NEXT_PUBLIC_API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
+export async function getMoviesUpcoming() {
+  const data = await fetch('https://api.themoviedb.org/3/movie/upcoming?language=es-ES&region=es', {
+    headers: {
+      Authorization: `Bearer ${NEXT_PUBLIC_API_KEY}`
+    }
+  });
+
+  const response = await data.json(); 
+  return response.results.slice(0,10);
+}
+
 export async function getMoviesNowPlaying() {
   const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=es-ES&page=1&region=ES', {
     headers: {
@@ -9,7 +20,7 @@ export async function getMoviesNowPlaying() {
   });
 
   const response = await data.json(); 
-  return response.results.slice(0,10);
+  return response.results
 }
 
 export async function getMovieGenres() {
