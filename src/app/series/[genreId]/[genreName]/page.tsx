cde16fa23dafde361fba33b83dac150e1fdab003
Nanna <nanaa.gonzalez@gmail.com>
2024-09-19 13:11:15 +0200

    Add series page and seriesByGenre

---
+++ src/app/series/[genreId]/[genreName]/page.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import { useParams } from 'next/navigation';
+import { Params } from 'next/dist/shared/lib/router/utils/route-matcher';
+import { useEffect, useState } from 'react';
+import VerticalCardCarousel from '@/components/verticalCarousel/VerticalCardMovie';
+import PaginationControlled from '@/components/Pagination';
+import { useSeriesByGenreId } from '@/store/seriesByGenreId';
+
+export default function SeriesByGenre() {
+  const {genreId, genreName} = useParams<Params>();
+  const [page, setPage] = useState<number>(1);
+  const decodedGenreName = decodeURIComponent(genreName || '');
+
+  const {
+    seriesByGenre,
+    pagesSeriesByGenre,
+    fetchSeriesByGenreId
+  } = useSeriesByGenreId(state => state);
+
+  const handleSetPage = (event: React.ChangeEvent<unknown>, value: number) => {
+    event.preventDefault();
+    setPage(value);
+  };
+
+  useEffect(() => {
+    fetchSeriesByGenreId(genreId, page);
+  }, [page, genreId, fetchSeriesByGenreId]);
+
+  return(
+    <main className='flex flex-col flex-grow'>
+      <h1 className='font-bold py-6 px-4 pt-6 text-2xl text-balance md:text-center lg:text-left lg:px-6'>
+        Lo mejor en {decodedGenreName}
+      </h1>
+      <section>
+        <ul className='flex flex-wrap px-4 gap-4 justify-center lg:px-6'>
+          {seriesByGenre.filter((serie) => serie.poster_path !== null).map((serie) => {
+            return(
+              <li key={serie.id}>
+                <VerticalCardCarousel media={serie} isSerie={true}/>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+      <div className='flex justify-center pb-4 pt-8 md:py-6 lg:py-8'>
+        <PaginationControlled page={page} handleSetPage={handleSetPage} maxPage={pagesSeriesByGenre}/>
+      </div>
+    </main>
+  );
+}
--- src/app/series/page.tsx
+++ src/app/series/page.tsx
@@ -2,19 +2,45 @@
 
 import WatchTrailer from '@/components/WatchTrailer';
 import MainCarousel from '@/components/mainCarousel/MainCarousel';
+import VerticalCarousel from '@/components/verticalCarousel/VerticalCarousel';
 import { useSeriesStore } from '@/store/series';
+import { useSeriesByGenreId } from '@/store/seriesByGenreId';
 import { EmblaOptionsType } from 'embla-carousel';
-import { useEffect } from 'react';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 export default function Series() {
-  const { seriesOfTheDay, fetchSeriesOfTheDay, serieGenres, fetchSerieGenre } = useSeriesStore(state => state);
+  const { 
+    seriesOfTheDay, 
+    fetchSeriesOfTheDay, 
+    serieGenres, 
+    fetchSerieGenre,
+    fetchOnAirSeries,
+    onAirSeries 
+  } = useSeriesStore(state => state);
+
+  const {
+    seriesByGenre,
+    fetchSeriesByGenreId
+  } = useSeriesByGenreId(state => state);
 
   const OPTIONS: EmblaOptionsType = { loop: true };
+
+  const [selectedGenreId, setSelectedGenreId] = useState<number>(10759);
+  const [selectedGenreName, setSelectedGenreName] = useState<string>('Action & Adventure');
+
+  const handleGenreClick = (id: number, name: string) => {
+    setSelectedGenreId(id);
+    setSelectedGenreName(name);
+    fetchSeriesByGenreId(id, 1);
+  };
   
   useEffect(() => {
     fetchSeriesOfTheDay();
     fetchSerieGenre(true);
-  }, [fetchSeriesOfTheDay, fetchSerieGenre]);
+    fetchOnAirSeries(1);
+    fetchSeriesByGenreId(10759, 1);
+  }, [fetchSeriesOfTheDay, fetchSerieGenre, fetchOnAirSeries, fetchSeriesByGenreId]);
 
   return(
     <main className="flex flex-col flex-grow">
@@ -25,6 +51,56 @@ export default function Series() {
         genres={serieGenres}
         isSerie={true}
       />
+      <section>
+        <div className="flex flex-col p-4 md:flex-row md:gap-4 md:pt-[30px] md:pb-5 lg:pt-9 lg:pb-6 lg:px-6 items-baseline">
+          <h2 className="font-bold text-lg md:text-xl lg:text-2xl">
+						¡No te lo pierdas! Se estrenan hoy
+          </h2>
+          <Link
+            href={'/estrenos-series'}
+            className="flex items-baseline text-sm text-gray hover:text-neonBlue"
+          >
+						Ver todo
+          </Link>
+        </div>
+        <VerticalCarousel
+          media={onAirSeries}
+          isSerie={true}
+          path={'/estrenos-series'}
+        />
+      </section>
+      <section>
+        <h2 className="font-bold text-lg p-4 md:text-xl md:pt-[30px] md:pb-5 lg:pt-9 lg:pb-6 lg:px-6 lg:text-2xl">
+					Historias únicas en cada género
+        </h2>
+        <ul className="flex flex-wrap gap-4 px-4 *:flex *:items-center *:flex-none *:px-4 *:py-2 *:rounded-3xl *:border-2 *:border-white *:cursor-pointer lg:px-6 lg:flex-wrap">
+          {serieGenres.map((genre) => {
+            const isSelected = genre.id === selectedGenreId;
+            return (
+              <li
+                key={genre.id}
+                className={`cursor-pointer px-4 py-2 rounded-3xl border-2 border-white ${
+                  isSelected
+                    ? 'bg-white text-black'
+                    : 'hover:bg-white hover:text-black'
+                }`}
+                onClick={() => handleGenreClick(genre.id, genre.name)}
+              >
+                {genre.name}
+              </li>
+            );
+          })}
+        </ul>
+        <div className="py-5">
+          {selectedGenreId && (
+            <VerticalCarousel
+              media={seriesByGenre}
+              isSerie={true}
+              path={`/series/${selectedGenreId}/${selectedGenreName}`}
+            />
+          )}
+        </div>
+      </section>
     </main>
   );
 }
